830b2f9fe56d7f74f72c4b23bc4203bc500385cd
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-10-17 18:47:45 +0200

    Implementing JWT

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User, Prisma } from '@prisma/client';
-import { UserRegisterDto } from './dto';
+import { UserLoginDto, UserRegisterDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {}
+
   async register({ email, password, name }: UserRegisterDto): Promise<User> {
     return this.prisma.user.create({
       data: { email, password, name },
     });
+  }
+
+  async login({ email, password }: UserLoginDto): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials Incorrect');
+
+    const passwordMatch = await argon.verify(user.password, password);
+    if (!passwordMatch) throw new ForbiddenException('Credentials Incorrect');
+
+    return user;
   }
 
   async updateUser(params: {
